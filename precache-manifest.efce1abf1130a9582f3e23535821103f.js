44313ed785fb9cf1e797239aea47b9e5b8917d28
jenmaz12 <jenmaz12@gmail.com>
2019-03-19 12:59:25 -0400

    Updates

---
+++ precache-manifest.efce1abf1130a9582f3e23535821103f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f8c06e794d0d30a018ab75a7f4e9487",
+    "url": "/clicky-game/static/media/background.1f8c06e7.jpg"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "23d6ad3652d72713e625",
+    "url": "/clicky-game/static/js/main.3a42edfd.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/clicky-game/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "23d6ad3652d72713e625",
+    "url": "/clicky-game/static/css/main.a348b262.chunk.css"
+  },
+  {
+    "revision": "b5a4192a722c49946fe356036c8ee31f",
+    "url": "/clicky-game/index.html"
+  }
+];
